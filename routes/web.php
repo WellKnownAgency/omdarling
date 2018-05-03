bfd68a3f154eb1181b385a875baaa7a759b8e0c9
TarasKim <kimtaras@gmail.com>
2018-05-03 18:12:19 -0400

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -12,54 +12,54 @@
 */
 
 Route::get('/', function () {
-  $posts = App\Post::orderBy('id', 'desc')->paginate(3);
+  $posts = App\Post::orderBy('id', 'desc')->where('status', 'PUBLISHED')->paginate(3);
     return view('about-me', compact('posts'));
 });
 
 Route::get('/travel', function () {
-  $posts = App\Post::where('category_id', 4)->orderBy('id', 'desc')->paginate(9);
+  $posts = App\Post::where('category_id', 4)->where('status', 'PUBLISHED')->orderBy('id', 'desc')->paginate(9);
     return view('travel', compact('posts'));
 });
 Route::get('/travel/{slug}', function($slug){
-	$post = App\Post::where('slug', '=', $slug)->where('category_id', 4)->firstOrFail();
+	$post = App\Post::where('slug', '=', $slug)->where('category_id', 4)->where('status', 'PUBLISHED')->firstOrFail();
 	return view('single', compact('post'));
 });
 
 Route::get('/yogi', function () {
-  $posts = App\Post::where('category_id', 4)->orderBy('id', 'desc')->paginate(9);
+  $posts = App\Post::where('category_id', 4)->where('status', 'PUBLISHED')->orderBy('id', 'desc')->paginate(9);
     return view('yogi', compact('posts'));
 });
 Route::get('/yogi/{slug}', function($slug){
-	$post = App\Post::where('slug', '=', $slug)->where('category_id', 4)->firstOrFail();
+	$post = App\Post::where('slug', '=', $slug)->where('status', 'PUBLISHED')->where('category_id', 4)->firstOrFail();
 	return view('single', compact('post'));
 });
 
 Route::get('/mindfulness', function () {
-  $posts = App\Post::where('category_id', 4)->orderBy('id', 'desc')->paginate(9);
+  $posts = App\Post::where('category_id', 4)->where('status', 'PUBLISHED')->orderBy('id', 'desc')->paginate(9);
     return view('mindfulness', compact('posts'));
 });
 Route::get('/mindfulness/{slug}', function($slug){
-	$post = App\Post::where('slug', '=', $slug)->where('category_id', 4)->firstOrFail();
+	$post = App\Post::where('slug', '=', $slug)->where('category_id', 4)->where('status', 'PUBLISHED')->firstOrFail();
 	return view('single', compact('post'));
 });
 
 Route::get('/body-and-soul', function () {
-  $posts = App\Post::where('category_id', 4)->orderBy('id', 'desc')->paginate(9);
+  $posts = App\Post::where('category_id', 4)->where('status', 'PUBLISHED')->orderBy('id', 'desc')->paginate(9);
     return view('body-and-soul', compact('posts'));
 });
 Route::get('/body-amd-soul/{slug}', function($slug){
-	$post = App\Post::where('slug', '=', $slug)->where('category_id', 4)->firstOrFail();
+	$post = App\Post::where('slug', '=', $slug)->where('category_id', 4)->where('status', 'PUBLISHED')->firstOrFail();
 	return view('single', compact('post'));
 });
 
 Route::get('food', function () {
-    $posts = App\Post::where('category_id', 3)->orderBy('id', 'desc')->paginate(9);
+    $posts = App\Post::where('category_id', 3)->where('status', 'PUBLISHED')->orderBy('id', 'desc')->paginate(9);
     //$categories = App\Category::all();
     return view('food', compact('posts'));
 });
 Route::get('/food/{slug}', function($slug){
-	$post = App\Post::where('slug', '=', $slug)->where('category_id', 3)->firstOrFail();
-  $posts = App\Post::orderBy('id', 'asc')->where('category_id', 3)->paginate(3);
+	$post = App\Post::where('slug', '=', $slug)->where('category_id', 3)->where('status', 'PUBLISHED')->firstOrFail();
+  $posts = App\Post::orderBy('id', 'asc')->where('category_id', 3)->where('status', 'PUBLISHED')->paginate(3);
 	return view('single', compact('post'), compact('posts'));
 });
 
