16db28cd3c4e8253c8db800f0c6c02c629d880c5
TarasKim <kimtaras@gmail.com>
2018-05-08 11:52:28 -0400

    initial commit

--- resources/views/mobile/about-me.blade.php
+++ resources/views/mobile/about-me.blade.php
@@ -7,5 +7,55 @@
 @section('keywords', '')
 
 @section('content')
+<header class="main-header">
+  <div class="container">
+    <div class="intro-text">
+      <div class="intro-l">
+        Hello
+      </div>
+      <div class="intro-l">
+        Darlings!
+      </div>
+      <hr align="left">
+      <div class="intro-sm">
+        my name is anastasia
+      </div>
+    </div>
+  </div>
+</header>
+<section class="container mg-b-20">
+  <div class="block-text-l">
+    About Me
+  </div>
+  <hr class="style-blue">
+  <div class="text-box">
+    <p><span style="font-weight:bold;">Thank you for finding time to step by at my
+       new blog OM!Darling.</span> I have created it with one simple but at the same time complicated idea…but first, who am I?</p>
+    <p style="font-weight:bold;">My name is Anastasia. I’m a yoga-lover and a spiritual seeker with
+    passion for all kinds of art, photography and travel.</p>
+    <p>You can find me in a meditation pose every day at 4:30 in the morning breathing slowly and not moving for an hour.
+      You can also find me at a jazz bar casually talking to my friends with a glass of rosé in my hand.</p>
+
+    <p>You can see me wearing leggings and sports bra all day long and rocking dresses and designer high heels at night.</p>
+
+    <p>My daily diet is raw vegan food but when I travel I won’t say “no” to croissants or pasta.</p>
+
+    <p style="font-weight: bold;">I CAN GO ON AND ON :)</p>
+  </div>
+</section>
+<section class="container">
+  <div class="column bg-image">
+    <div class="container-right-half">
+      <div class="block-text-3">
+        <h2><span class="quotes">ʺ</span> Yoga Studio <br> is my church.</br>
+            ocean is my drug.<br>
+            art is my religion.<span>ʺ</span></h2>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
 
 @stop
