581c8d7851607e6aec3e5853a8f4372b0af4b026
TarasKim <kimtaras@gmail.com>
2018-05-08 20:37:48 -0400

    initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
           accessToken: '277022847.1677ed0.a98d326b3caf423ba5b1d7003bd0cb34',
           sortBy: 'most-recent',
           limit: 8,
-          template: '<div><a href="{{link}}" target="_blank"><img src="{{model.images.standard_resolution.url}}" orientation="{{square}}" class="insta"/></a></div>',
+          template: '<div><a href="{{link}}" target="_blank"><img src="{{model.images.thumbnail.url}}" orientation="{{square}}" class="insta"/></a></div>',
       });
       feed.run();
 });
