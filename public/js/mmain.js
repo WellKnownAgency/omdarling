c24394f4165fd46f7ba78e071cc2c9fbb7ba0102
TarasKim <kimtaras@gmail.com>
2018-05-08 20:20:52 -0400

    initial commit

--- public/js/mmain.js
+++ public/js/mmain.js
@@ -7,12 +7,12 @@ $(document).ready(function(event){
 $(document).ready(function() {
   var feed = new Instafeed({
           get: 'user',
-          userId: '248274392',
-          clientId: '474194880b8845819aba26a07dc122fd',
-          accessToken: '248274392.1677ed0.8a29b5a11e5f4e0f8b8e6ddd0933ac4c',
+          userId: '277022847',
+          clientId: '01289662bc0445a8a9c6d0bccf3b1d26',
+          accessToken: '277022847.1677ed0.a98d326b3caf423ba5b1d7003bd0cb34',
           sortBy: 'most-recent',
           limit: 8,
-          template: '<div><a href="{{link}}" target="_blank"><img src="{{model.images.standard_resolution.url}}" class="insta"/></a></div>',
+          template: '<div><a href="{{link}}" target="_blank"><img src="{{model.images.standard_resolution.url}}" orientation="{{square}}" class="insta"/></a></div>',
       });
       feed.run();
 });
