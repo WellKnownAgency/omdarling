2646d29b5189d58b93e995c95856ca482e0d6e1e
TarasKim <kimtaras@gmail.com>
2018-05-16 08:53:00 -0400

    initial commit

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/css/main.css">
 <link rel="stylesheet" href="/web-fonts-with-css/css/fontawesome-all.css">
-<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,500" rel="stylesheet">
 
 <meta name="description" content="@yield('description')">
 <meta name="keywords" content="@yield('keywords')">
