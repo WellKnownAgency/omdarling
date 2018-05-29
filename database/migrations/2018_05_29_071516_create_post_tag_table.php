d58bf9e49c4f343d0040fb447b02a216e374ca74
TarasKim <kimtaras@gmail.com>
2018-05-29 05:26:37 -0400

    initial commit

--- database/migrations/2018_05_29_071516_create_post_tag_table.php
+++ database/migrations/2018_05_29_071516_create_post_tag_table.php
@@ -1,27 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreatePostTagTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-      Schema::create('post_tag', function (Blueprint $table) {
-          $table->increments('id');
-          $table->integer('post_id')->unsigned();
-          $table->foreign('post_id')->reference('id')->on('posts');
-          $table->integer('tag_id')->unsigned();
-          $table->foreign('tag_id')->reference('id')->on('tags');
-      });
-    }
 
+  public function up()
+  {
+     Schema::create('post_tag', function (Blueprint $table) {
+             $table->increments('id');
+             $table->integer('post_id')->unsigned();
+             $table->integer('tag_id')->unsigned();
+
+             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+         });
+       }
     /**
      * Reverse the migrations.
      *
@@ -29,6 +25,6 @@ class CreatePostTagTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post_tag');
+        Schema::drop('post_tag');
     }
 }
