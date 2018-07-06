b353b83f7d295a77c3e9418753f3072abc0c1f04
TarasKim <kimtaras@gmail.com>
2018-07-06 09:20:14 -0400

    initial commit

--- resources/views/partials/_headblog.blade.php
+++ resources/views/partials/_headblog.blade.php
@@ -3,7 +3,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/css/main.css">
-<link rel="stylesheet" href="/css/toastr.css">
+<link rel="stylesheet" href="/css/toast.css">
 <link rel="stylesheet" href="/web-fonts-with-css/css/fontawesome-all.css">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
