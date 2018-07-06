9c6ed007228a78223ddc44db8a69e20ff953118d
TarasKim <kimtaras@gmail.com>
2018-07-06 09:27:50 -0400

    initial commit

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,6 +11,7 @@
     </div>
     @include('partials/_subscription')
     @include('partials/_footer')
+    <div style="font-size: 10px; float:right;">by <a style="text-decoration: none; color:black;" href="https://wknown.com/web-design">WellKnown Agency</a></div>
     @include('partials/_js')
   </body>
 </html>
--- resources/views/mobile/main.blade.php
+++ resources/views/mobile/main.blade.php
@@ -9,6 +9,7 @@
     @yield('content')
     @include('mobile/partials/_subscription')
     @include('mobile/partials/_footer')
+    <div style="font-size: 10px; float:right;">by <a style="text-decoration: none; color:black;" href="https://wknown.com/web-design">WellKnown Agency</a></div>
     @include('mobile/partials/_js')
   </body>
 </html>
