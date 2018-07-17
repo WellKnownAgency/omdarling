3acd59dd57a769c12d837fb11f78be236e7d04d6
TarasKim <kimtaras@gmail.com>
2018-07-17 16:12:27 -0400

    initial commit

--- resources/views/about-me.blade.php
+++ resources/views/about-me.blade.php
@@ -111,7 +111,7 @@
 <section class="container-sm profile">
       <hr class="style-block5 mg-bot">
       <div class="profile-pic">
-        <img src="/images/profile.png"/>
+        <img style="border-radius: 50%;" width="229px" src="/images/profile.jpg"/>
         <img src="/images/anastasia.png"/>
       </div>
       <hr class="style-block5 mg-top">
