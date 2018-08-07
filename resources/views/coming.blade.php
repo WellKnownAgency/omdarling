83c482b638feff2448c453bec2fbd427e737b76b
TarasKim <kimtaras@gmail.com>
2018-08-06 20:06:35 -0400

    initial commit

--- resources/views/coming.blade.php
+++ resources/views/coming.blade.php
@@ -95,7 +95,7 @@ button.subscribe:hover {
 
 <script>
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 6, 2018 12:00:00").getTime();
+var countDownDate = new Date("Aug 15, 2018 12:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
