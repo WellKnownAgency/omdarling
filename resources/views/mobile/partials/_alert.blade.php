ad6492b610c0c5a35daee402ff791bf8def2d52e
Taras <taraskim@wknown.com>
2019-01-11 17:53:41 -0500

    initail comit

--- resources/views/mobile/partials/_alert.blade.php
+++ resources/views/mobile/partials/_alert.blade.php
@@ -1,5 +1,5 @@
 @if (Session::has('success'))
-
+toastr.success('{{ Session::get('success') }}')
   <div id="toast-container" class="toast-top-rightt">
     <div class="toast toast-success" aria-live="polite">
       <div class="toast-message">{{ Session::get('success') }}</div>
