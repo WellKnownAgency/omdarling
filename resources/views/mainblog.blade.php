2b87c2f24608d44989bdd18f1e296330714ab94e
Taras <taraskim@wknown.com>
2019-01-11 18:27:24 -0500

    initail comit

--- resources/views/mainblog.blade.php
+++ resources/views/mainblog.blade.php
@@ -13,5 +13,27 @@
     @include('partials/_footer')
     <div style="font-size: 10px; float:right;">by <a style="text-decoration: none; color:black;" href="https://wknown.com/web-design">WellKnown Agency</a></div>
     @include('partials/_js')
+    <script>
+      @if(Session::has('message'))
+        var type = "{{ Session::get('alert-type', 'info') }}";
+        switch(type){
+            case 'info':
+                toastr.info("{{ Session::get('message') }}");
+                break;
+
+            case 'warning':
+                toastr.warning("{{ Session::get('message') }}");
+                break;
+
+            case 'success':
+                toastr.success("{{ Session::get('message') }}");
+                break;
+
+            case 'error':
+                toastr.error("{{ Session::get('message') }}");
+                break;
+        }
+      @endif
+    </script>
   </body>
 </html>
--- resources/views/mobile/mainabout.blade.php
+++ resources/views/mobile/mainabout.blade.php
@@ -11,5 +11,27 @@
     @include('mobile/partials/_footer')
     <div style="font-size: 10px; float:right; margin-right:15px;">by <a style="text-decoration: none; color:black;" href="https://wknown.com/web-design" target="_blank">WellKnown Agency</a></div>
     @include('mobile/partials/_js')
+    <script>
+      @if(Session::has('message'))
+        var type = "{{ Session::get('alert-type', 'info') }}";
+        switch(type){
+            case 'info':
+                toastr.info("{{ Session::get('message') }}");
+                break;
+
+            case 'warning':
+                toastr.warning("{{ Session::get('message') }}");
+                break;
+
+            case 'success':
+                toastr.success("{{ Session::get('message') }}");
+                break;
+
+            case 'error':
+                toastr.error("{{ Session::get('message') }}");
+                break;
+        }
+      @endif
+    </script>
   </body>
 </html>
